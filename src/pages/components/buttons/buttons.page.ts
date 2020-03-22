107ea3e6b4f2d56b643d8d506696be0bdcad281f
Renan Cavalcante <renan.cav.ti@gmail.com>
2020-03-22 10:22:55 -0300

    Buttons with some icons

--- src/pages/components/buttons/buttons.page.ts
+++ src/pages/components/buttons/buttons.page.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-buttons',
@@ -8,13 +7,9 @@ import { NavController } from '@ionic/angular';
 })
 export class ButtonsPage implements OnInit {
 
-  constructor(public navCtrl: NavController) { }
+  constructor( ) { }
 
   ngOnInit() {
-  }
-
-  backHome() {
-    this.navCtrl.back()
   }
 
 }
