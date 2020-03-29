c196bbd12d54173867e4c5fc2b9c9a6f0edead5b
Renan Cavalcante <renan.cav.ti@gmail.com>
2020-03-29 09:53:40 -0300

    WIP: Toast page added

--- src/pages/components/toast/toast.page.ts
+++ src/pages/components/toast/toast.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-toast',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ToastPage implements OnInit {
 
-  constructor() { }
+  constructor(public toastCtrl: ToastController) { }
 
   ngOnInit() {
   }
 
+  async showToast() {
+    const toast = await this.toastCtrl.create({
+      message: 'This is a toast',
+      position: 'top',
+      cssClass: 'my-toast',
+      buttons: [
+        {
+          text: 'OK',
+          role: 'close',
+          handler: () => console.log("OK clicked"),
+        }
+      ]
+    })
+
+    await toast.present()
+
+    setTimeout (() => toast.message = '3 seconds has passed', 3000)
+
+    toast.onWillDismiss().then(() => console.log("Being closed"))
+    toast.onDidDismiss().then(() => console.log("Was closed"))
+  }
 }
