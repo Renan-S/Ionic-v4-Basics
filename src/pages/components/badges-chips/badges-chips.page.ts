27d53835e9fc3f28ae43df47b546bc656e22f6ef
Renan Cavalcante <renan.cav.ti@gmail.com>
2020-03-29 16:09:00 -0300

    WIP: Badge and Chips added

--- src/pages/components/badges-chips/badges-chips.page.ts
+++ src/pages/components/badges-chips/badges-chips.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-badges-chips',
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BadgesChipsPage implements OnInit {
 
+  
+  public friendsNum: number = Math.floor(Math.random()*10);
+
   constructor() { }
 
   ngOnInit() {
   }
-
 }
